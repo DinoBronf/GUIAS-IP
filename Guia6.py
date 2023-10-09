ecc39c787727823150d0fac2df5e78a6c3e5de49
DinoBronf <142249369+DinoBronf@users.noreply.github.com>
2023-10-09 12:00:20 -0300

    Add files via upload

--- Guia6.py
+++ Guia6.py
@@ -1,4 +1,8 @@
+from ast import Str
 import math
+from re import X
+import re
+from tokenize import String
 #Ejercicio 1
 #1.1
 def imprimir_hola_mundo() -> None:
@@ -16,12 +20,6 @@ print(perimetro())
 
 def es_multiplo_de(x: int, y: int) -> bool:
     return x % y == 0 
-         
-def devolver_el_doble_si_es_par(x:int) -> int:
-    if x % 2 == 0 :
-      return x * 2
-    else : 
-      return x 
 
 def pares_10_40() -> None:
     x == 10
@@ -108,3 +106,61 @@ def sirve_pino(pinito) -> bool:
 def es_peso_util(pinito) -> bool:
     return max (pinito,1000) == 1000 and min(pinito,400) == 400
 #Ejercicio 5
+#5.1
+def devolver_el_doble_si_es_par(x:int) -> int:
+    if x % 2 == 0 :
+      return x * 2
+    else : 
+      return x 
+#5.2
+def devolver_valor_si_es_par_sino_el_que_sigue(numeringui) -> int:
+   if numeringui % 2 == 0:
+      return numeringui
+   else: 
+      return numeringui + 1
+#5.3
+def devolver_el_doble_si_es_multiplo3_el_triple_si_es_multiplo9(num) -> int:
+   if num % 9 == 0:
+      return num * 3
+   if num % 3 == 0:
+      return num * 2
+   else:
+      return num
+#5.4
+def lindo_nombre(nombre) -> str:
+   if len(nombre) >= 5:
+      return 'tu nombre tiene muchas letras!'
+   else:
+      return 'Tu nombre tiene menos de 5 caracteres'
+#5.5
+def elRango(num) -> str:
+   if num < 5:
+      return 'menor a 5'
+   if 10 < num < 20:
+      return 'entre 10 y 20'
+   if num > 20:
+      return 'mayor a 20'
+   else: 
+      return num  
+#5.6
+def vacaciones_o_laburo(g,edad) -> str:
+    if edad >= 65:
+       return 'vacacionees'
+    if g == 'm' and edad < 65:
+       return 'a laburar'
+    if g == 'f' and edad >= 60:
+       return 'vacacionees' 
+    if 18 < edad < 60:
+       return 'a laburar'
+    if edad <= 18:
+       return 'vacacionees'       
+#Ejercicio 6
+#6.1
+def nums_1_al_10() -> None:
+   x = 1 
+   while (x != 11):
+    print(x) 
+    x = x +1  
+nums_1_al_10()
+#6.2
+ 
